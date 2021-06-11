23dcc3eaf29ff58b0fa8f8f2c5ac0d24357a040a
filipefcintra <filipefcintra@gmail.com>
2021-06-11 01:06:00 +0100

    disapointed

--- js/memory.js
+++ js/memory.js
@@ -1,19 +1,39 @@
 class MemoryGame {
   constructor(cards) {
     this.cards = cards;
-    // add the rest of the class properties here
+    this.pickedCards = []
+    this.pairsClicked = 0
+    this.pairsGuessed = 0
+    
   }
 
   shuffleCards() {
-    // ... write your code here
-  }
+    this.cards.sort(function (a, b) {
+      return Math.random() - 0.5
+    })
+    };
+    
 
   checkIfPair(card1, card2) {
-    // ... write your code here
-  }
+    if(card1 === card2) {
+      this.pairsGuessed += 1
+      this.pairsClicked += 1
+      return true
+    }
+    else {
+      this.pairsClicked += 1
+      return false
+    }
+    
+    };
 
   checkIfFinished() {
-    // ... write your code here
+    if(this.pairsGuessed === (this.cards.length / 2)) {
+      return true
+    }
+    else {
+      return false
+    }
   }
 }
 
